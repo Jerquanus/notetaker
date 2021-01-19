e07b103d09e57388a8bd526204b9c01bf27e1694
Jerquanus Swann <jerquanus@gmail.com>
2021-01-19 13:56:43 -0500

    Adding routing

---
+++ Develop/server.js
@@ -0,0 +1,27 @@
+//  create express dependancy
+var express = require("express");
+// =========
+
+// tell node to create express server
+var app = express();
+
+
+// =========
+// create listening port
+var PORT = process.env.PORT || 8080;
+
+// =========
+// handle data parsing 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// =========
+// create routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// =========
+// start listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
