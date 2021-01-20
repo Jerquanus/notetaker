85c443231d2047dd2544065bb4515e2a3eeaf6fd
Jerquanus Swann <jerquanus@gmail.com>
2021-01-19 23:24:50 -0500

    start script added, html error corrected

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,22 +1,44 @@
 // Link the Data
 
-var noteData = require("../server");
+var notesData = require("../db/db.json");
+var fs = require('fs')
 
 // ============
 // start routing
-
 module.exports = function(app) {
+
     // API get
     app.get("/api/notes", function(req, res) {
-      res.json(noteData);
+
+        console.log('NOTES DATA--->', notesData)
+        res.json(notesData);
     });
   
     // API POST request
   
     app.post("/api/notes", function(req, res) {
-        noteData.push(req.body);
-        res.json(true);
+        const newNote = req.body;
+        const data = json.stringify(newNote)
+        notesData.push(newNote);
+        fs.writeFile('./db/db.json',data,function(err){
+            if(err) console.log(err)
+            res.json(newNote);
+        })
     });
+
+
+
+    // app.post("/api/notes", function)
+
+
+
+
+
+
+
+
+
+
   
   };
   
--- Develop/server.js
+++ Develop/server.js
@@ -5,7 +5,6 @@ var express = require("express");
 // tell node to create express server
 var app = express();
 
-
 // =========
 // create listening port
 var PORT = process.env.PORT || 8080;
@@ -14,7 +13,7 @@ var PORT = process.env.PORT || 8080;
 // handle data parsing 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static("public"));
 // =========
 // create routes
 require("./routes/apiRoutes")(app);
@@ -25,3 +24,4 @@ require("./routes/htmlRoutes")(app);
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
   });
+
