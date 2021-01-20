cd12fc8c4c3b49f102977e26a6bf3414b6162c47
Jerquanus Swann <jerquanus@gmail.com>
2021-01-20 16:25:01 -0500

    need to correct stringify, and need to add function and for loop to delete

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -15,6 +15,7 @@ module.exports = function(app) {
     });
   
     // API POST request
+    // fix the code below you want to stringify to the file not the notes
   
     app.post("/api/notes", function(req, res) {
         const newNote = req.body;
@@ -28,8 +29,20 @@ module.exports = function(app) {
 
 
 
-    // app.post("/api/notes", function)
-
+    // re-use the code below to create a function that searches for the id to delete it
+    app.get("/api/characters/:character", function(req, res) {
+        var chosen = req.params.character;
+      
+        console.log(chosen);
+      
+        for (var i = 0; i < characters.length; i++) {
+          if (chosen === characters[i].routeName) {
+            return res.json(characters[i]);
+          }
+        }
+      
+        return res.json(false);
+      });
 
 
 
