07567fdf6f206ae370f439c86de40e17930cf1e6
Jerquanus Swann <jerquanus@gmail.com>
2021-01-21 00:06:14 -0500

    Testing Notes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -21,7 +21,7 @@ module.exports = function(app) {
         const newNote = req.body;
         const data = json.stringify(newNote)
         notesData.push(newNote);
-        fs.writeFile('./db/db.json',data,function(err){
+        fs.writeFile('./public/notes.html',data,function(err){
             if(err) console.log(err)
             res.json(newNote);
         })
@@ -30,7 +30,7 @@ module.exports = function(app) {
 
 
     // re-use the code below to create a function that searches for the id to delete it
-    app.get("/api/characters/:character", function(req, res) {
+    app.get("/api/notes", function(req, res) {
         var chosen = req.params.character;
       
         console.log(chosen);
